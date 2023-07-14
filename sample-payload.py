45280a1751e83aab0539f2368aef2147ef75f4ac
Ramya Oruganti <45637559+raorugan@users.noreply.github.com>
2023-07-14 23:56:36 +0530

    File  commit

---
+++ sample-payload.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+url = "http://localhost:7071/query"
+data = {
+   "queries" : [
+       {
+            "query": "Where is my keda file?"  ,
+            #"filter" : {"author" : "Bob"}  ,
+            "top_k"  : 1
+            
+        }
+   ]
+   
+}
+
+
+
+response = requests.post(url, data=json.dumps(data))
+#print(response.content)
+
+
+
+json_response = response.json()
+
+data = json_response['results'][0]['results']
+
+for item in data:
+    print(item['metadata']['source_id'])
+
+
