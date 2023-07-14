fcb588ac3b122f53008cdfd3f7264a7d458633dd
Ramya Oruganti <45637559+raorugan@users.noreply.github.com>
2023-07-14 23:44:46 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+
+FROM python:3.10 as requirements-stage
+
+WORKDIR /tmp
+
+RUN pip install --upgrade pip
+RUN pip install poetry
+
+COPY ./pyproject.toml ./poetry.lock* /tmp/
+
+
+RUN poetry export -f requirements.txt --output requirements.txt --without-hashes
+
+#FROM python:3.10
+FROM mcr.microsoft.com/azure-functions/python:4-python3.10
+
+ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
+    AzureFunctionsJobHost__Logging__Console__IsEnabled=true
+
+#WORKDIR /code
+
+RUN apt-get update && apt-get -y install libpq-dev gcc
+
+COPY --from=requirements-stage /tmp/requirements.txt requirements.txt
+RUN pip install --upgrade pip
+
+
+
+#RUN pip install psycopg2
+
+#RUN pip install --no-cache-dir --upgrade -r requirements.txt
+RUN python3 -m pip install  -r requirements.txt
+
+COPY . /home/site/wwwroot
+
+#COPY . /code/
+
+# Heroku uses PORT, Azure App Services uses WEBSITES_PORT, Fly.io uses 8080 by default
+#CMD ["sh", "-c", "uvicorn server.main:app --host 0.0.0.0 --port ${PORT:-${WEBSITES_PORT:-8080}}"]
---
+++ server/chatgptplugin.py
@@ -0,0 +1,138 @@
+# This is a version of the main.py file found in ../../../server/main.py for testing the plugin locally.
+# Use the command `poetry run dev` to run this.
+from typing import Optional
+import uvicorn
+from fastapi import FastAPI, File, Form, HTTPException, Body, UploadFile
+from loguru import logger
+
+
+from models.api import (
+    DeleteRequest,
+    DeleteResponse,
+    QueryRequest,
+    QueryResponse,
+    UpsertRequest,
+    UpsertResponse,
+)
+from datastore.factory import get_datastore
+from services.file import get_document_from_file
+
+from starlette.responses import FileResponse
+
+from models.models import DocumentMetadata, Source
+from fastapi.middleware.cors import CORSMiddleware
+
+from  . import wellknownroutes
+
+
+
+app = FastAPI()
+"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://chat.openai.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
+"""
+
+#app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
+app.include_router(wellknownroutes.wellknown)
+
+#PORT = 7071
+# Create a sub-application, in order to access just the query endpoint in an OpenAPI schema, found at http://0.0.0.0:8000/sub/openapi.json when the app is running locally
+
+
+
+@app.post(
+    "/upsert-file",
+    response_model=UpsertResponse,
+)
+async def upsert_file(
+    file: UploadFile = File(...),
+    metadata: Optional[str] = Form(None),
+):
+    datastore = await get_datastore()
+    try:
+        metadata_obj = (
+            DocumentMetadata.parse_raw(metadata)
+            if metadata
+            else DocumentMetadata(source=Source.file)
+        )
+    except:
+        metadata_obj = DocumentMetadata(source=Source.file)
+
+    document = await get_document_from_file(file, metadata_obj)
+
+    try:
+        ids = await datastore.upsert([document])
+        return UpsertResponse(ids=ids)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail=f"str({e})")
+
+
+@app.post(
+    "/upsert",
+    response_model=UpsertResponse,
+)
+async def upsert(
+    request: UpsertRequest = Body(...),
+):
+    datastore = await get_datastore()
+    try:
+        ids = await datastore.upsert(request.documents)
+        return UpsertResponse(ids=ids)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@app.post("/query", response_model=QueryResponse)
+async def query_main(request: QueryRequest = Body(...)):
+    datastore = await get_datastore()
+    try:
+        results = await datastore.query(
+            request.queries,
+        )
+        return QueryResponse(results=results)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+@app.delete(
+    "/delete",
+    response_model=DeleteResponse,
+)
+async def delete(
+    request: DeleteRequest = Body(...),
+):
+    datastore = await get_datastore()
+    if not (request.ids or request.filter or request.delete_all):
+        raise HTTPException(
+            status_code=400,
+            detail="One of ids, filter, or delete_all is required",
+        )
+    try:
+        success = await datastore.delete(
+            ids=request.ids,
+            filter=request.filter,
+            delete_all=request.delete_all,
+        )
+        return DeleteResponse(success=success)
+    except Exception as e:
+        logger.error(e)
+        raise HTTPException(status_code=500, detail="Internal Service Error")
+
+
+#@app.on_event("startup")
+#async def startup():
+ #   global datastore
+  #  datastore = await get_datastore()
+
+
+#def start():
+ #   uvicorn.run("local_server.main:app", host="localhost", port=7071, reload=True)
+
